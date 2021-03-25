cf76ce9613cac7481988a5c530727e52a230efb0
nishantgupta <nishantguptask2499@gmail.com>
2021-03-25 20:59:51 +0545

    changes to validation

--- app/Http/Controllers/Api/V1/LoginController.php
+++ app/Http/Controllers/Api/V1/LoginController.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
 
 class LoginController extends Controller
 {
